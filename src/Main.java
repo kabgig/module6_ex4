50db50d8badedddfa75da11c1995f3b291d76bc3
Ansar Ismagilov <ansar89kab@bk.ru>
2022-12-22 16:29:14 +0100

    commit 3 - program version with scanner

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String number = "12345678";
+        Scanner s = new Scanner(System.in);
+        System.out.print("inter a big number: ");
+
+        String number = s.next();
         List<Character> list = new ArrayList<>();
 
         for (int i = number.length()-1; i >=0 ; i--) {
